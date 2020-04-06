7eff6b64c730e538ec5c4ce4e57550a8f20b851b
Inklud <61893998+Inklud@users.noreply.github.com>
2020-04-06 16:18:13 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+    <fork-this/>
+    <div class="main">
+      <navbar/>
+      <nuxt/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Navbar";
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style scoped>
+.app {
+  height: 100vh;
+  width: 100vw;
+}
+.main {
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 30px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <super-secret-div v-if="$auth.loggedIn"/>
+    <div class="content">
+      <h1>Hei, {{ $auth.loggedIn ? $auth.user.email : 'friend' }}!</h1>
+      <p v-if="$auth.loggedIn">Now try going to our
+        <nuxt-link to="/secret" class="link">super secret page</nuxt-link>!
+      </p>
+      <p v-else>
+        So, Maybe you want to
+        <a @click="$auth.login()" class="link">sign in</a> and see what happens?
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import SuperSecretDiv from "~/components/SuperSecretDiv";
+
+export default {
+  components: {
+    SuperSecretDiv
+  }
+};
+</script>
+
+<style scoped>
+.content {
+  max-width: 750px;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
